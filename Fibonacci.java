a2fccc6b5f8cae2e4407b2bf23a7de9dee692ebe
Jaime Maldonado <jmem716@gmail.com>
2021-09-06 13:03:16 -0400

    Add files via upload
    Implemented JavaDocs

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,6 +1,10 @@
 
 public class Fibonacci {
 
+    /**
+     * Gets the the first main entry and calls the sum function based on the int provided. 
+     * It then prints out the returned final number from sum().
+     */
 	public static void main(String[] args) {
 		String s1;
 		int num = 10;
@@ -23,6 +27,11 @@ public class Fibonacci {
 
 	} 
 	
+    /**
+     * Receives an int as an argument which it uses to then add up using recursive function calling use.
+     * 
+     * @return The nth term of the Fibonacci sequence when 0 is reached
+     */
 	public static int sum(int k) {
 	    if (k > 0) {
 	      return k + sum(k - 1);
